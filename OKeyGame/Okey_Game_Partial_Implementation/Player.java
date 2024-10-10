8dbe2e80683ae25862754c2330c45bcfa42b9e4d
Ege <egeural2005@gmail.com>
2024-10-10 18:38:20 +0300

    Completed player class with 3 TODO methods

--- OKeyGame/Okey_Game_Partial_Implementation/Player.java
+++ OKeyGame/Okey_Game_Partial_Implementation/Player.java
@@ -13,7 +13,18 @@ public class Player {
      * TODO: removes and returns the tile in given index
      */
     public Tile getAndRemoveTile(int index) {
-        return null;
+        if(index==0||index>numberOfTiles){
+            return null;
+        }
+        Tile removedTile=playerTiles[index];
+            for(int i=index;i<numberOfTiles;i++){
+                playerTiles[i]=playerTiles[i+1];
+
+            }
+            playerTiles[numberOfTiles-1]=null;
+            numberOfTiles--;
+            return removedTile;
+
     }
 
     /*
@@ -22,6 +33,21 @@ public class Player {
      * make sure playerTiles are not more than 15 at any time
      */
     public void addTile(Tile t) {
+
+        if(numberOfTiles<15){
+            int currentIndex = 0;
+        
+            while (currentIndex < numberOfTiles && playerTiles[currentIndex].compareTo(t) < 0) {
+            currentIndex++;
+            }
+                for (int i = numberOfTiles; i > currentIndex; i--) {
+                    playerTiles[i] = playerTiles[i - 1];
+                }
+                playerTiles[currentIndex]=t;
+
+                numberOfTiles++;
+               
+        }
 
     }
 
@@ -32,7 +58,35 @@ public class Player {
      * @return
      */
     public boolean isWinningHand() {
-        return false;
+        
+        if (numberOfTiles < 12) {
+            return false;
+        }
+
+        int chainCount = 0;
+        int currentChainLength = 1;
+
+        for (int i = 1; i < numberOfTiles; i++) {
+
+            if (playerTiles[i].canFormChainWith(playerTiles[i - 1])) {
+                currentChainLength++;
+
+                if (currentChainLength == 4) {
+                    chainCount++;
+                    currentChainLength = 0;
+                }
+            } else {
+                currentChainLength = 1;
+            }
+        }
+        
+        if(chainCount>=3){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
     }
 
     public int findPositionOfTile(Tile t) {
